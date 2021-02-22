63063dd1743c763f3140fd89d65505ac317e251e
MINGKYME <admin@mingky.me>
2021-02-22 21:58:13 +0900

    Test

--- extension.js
+++ extension.js
@@ -6,26 +6,30 @@ const vscode = require('vscode');
 function activate(context) {
 
 	let disposable = vscode.commands.registerCommand('AutoIncreasement.Change', function () {
-	});
-	const editor = vscode.window.activeTextEditor;
-	const document = editor.document;
-
-	if (editor) {
-		var firstLine = editor.document.lineAt(0);
-		var lastLine = editor.document.lineAt(editor.document.lineCount - 1);
-		var textRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
-		let result = document.getText(textRange);
-		let index = 0;
-		while (result.indexOf("{@}") > 0) {
-			result = result.replace("{@}", index.toString());
-			index++;
+		const editor = vscode.window.activeTextEditor;
+		const document = editor.document;
+	
+		if (editor) {
+			var firstLine = editor.document.lineAt(0);
+			var lastLine = editor.document.lineAt(editor.document.lineCount - 1);
+			var textRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
+			var result = document.getText(textRange);
+			var index = 0;
+			while (result.indexOf("`@`") > 0) {
+			vscode.window.showInformationMessage(index);
+				result = result.replace("`@`", index.toString());
+				index++;
+			}
+			editor.edit(doc => {
+				doc.replace(textRange, result);
+			});
+			// vscode.window.showInformationMessage(result);
 		}
-		editor.edit(doc => {
-			doc.replace(textRange, result);
-		})
-	}
-	context.subscriptions.push(disposable);
+	});
+	
 
+	
+	context.subscriptions.push(disposable);
 }
 function deactivate() { }
 module.exports = {
