675a18d7883fc50f98a1423a4eb563b96ca9be1a
MINGKYME <admin@mingky.me>
2021-02-28 12:55:26 +0900

    시작 인덱스 지정 기능 추가

--- extension.js
+++ extension.js
@@ -6,29 +6,41 @@ const vscode = require('vscode');
 function activate(context) {
 
 	let disposable = vscode.commands.registerCommand('AutoIncreasement.Change', function () {
-		const editor = vscode.window.activeTextEditor;
-		const document = editor.document;
-	
-		if (editor) {
-			let firstLine = editor.document.lineAt(0);
-			let lastLine = editor.document.lineAt(editor.document.lineCount - 1);
-			let textRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
-			let result = document.getText(textRange);
-			let index = 0;
-			while (result.indexOf('{@}') >= 0) {
-				result = result.replace("{@}", index.toString());
-				index++;
-			}
-			vscode.window.showInformationMessage(index.toString()+"개 변경됐습니다.");
-			editor.edit(doc => {
-				doc.replace(textRange, result);
-			});
-			deactivate();
-		}
-	});
-	
+		vscode.window.showInputBox({ prompt: 'Start Index', placeHolder: '0', value: '0' })
+			.then(function (value) {
+				value = parseInt(value);
+				if (!isNaN(value)) {
 
-	
+					const editor = vscode.window.activeTextEditor;
+					const document = editor.document;
+
+					if (editor) {
+						let firstLine = editor.document.lineAt(0);
+						let lastLine = editor.document.lineAt(editor.document.lineCount - 1);
+						let textRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
+						let result = document.getText(textRange);
+						let index = value;
+						while (result.indexOf('{@}') >= 0) {
+							result = result.replace("{@}", index.toString());
+							index++;
+						}
+						vscode.window.showInformationMessage((index - value).toString() + " Changed");
+						editor.edit(doc => {
+							doc.replace(textRange, result);
+						});
+						deactivate();
+					}
+				}
+				else
+				{
+					vscode.window.showErrorMessage("Not A Number");
+				}
+			})
+
+	});
+
+
+
 	context.subscriptions.push(disposable);
 }
 function deactivate() { }
