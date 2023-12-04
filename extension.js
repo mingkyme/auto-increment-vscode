fd630a03b0618da65546b8060964f7cc16fb4d6c
Mingky Kim <admin@mingky.me>
2023-12-04 10:43:30 +0900

    ✨ 자리수와 간격 기능 추가

--- extension.js
+++ extension.js
@@ -1,41 +1,34 @@
+const { rejects } = require('assert');
 const vscode = require('vscode');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+	let disposable = vscode.commands.registerCommand('AutoIncreasement.Change', async function () {
+		const editor = vscode.window.activeTextEditor;
+		const document = editor.document;
+		if (!editor) {
+			vscode.window.showInformationMessage('No editor is active.');
+			return;
+		}
+		let startNumber = parseInt(await vscode.window.showInputBox({ prompt: 'Start Index', placeHolder: '0', value: '0', validateInput: (value) => { parseInt(value); return isNaN(value) ? 'Not A Number' : null; } }));
+		let digitNumber = parseInt(await vscode.window.showInputBox({ prompt: 'Digit Number', placeHolder: '1', value: '1', validateInput: (value) => { parseInt(value); return isNaN(value) ? 'Not A Number' : null; } }));
+		let stepNumber = parseInt(await vscode.window.showInputBox({ prompt: 'Step Number', placeHolder: '1', value: '1', validateInput: (value) => { parseInt(value); return isNaN(value) ? 'Not A Number' : null; } }));
 
-	let disposable = vscode.commands.registerCommand('AutoIncreasement.Change', function () {
-		vscode.window.showInputBox({ prompt: 'Start Index', placeHolder: '0', value: '0' })
-			.then(function (value) {
-				value = parseInt(value);
-				if (!isNaN(value)) {
-
-					const editor = vscode.window.activeTextEditor;
-					const document = editor.document;
-
-					if (editor) {
-						let firstLine = editor.document.lineAt(0);
-						let lastLine = editor.document.lineAt(editor.document.lineCount - 1);
-						let textRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
-						let result = document.getText(textRange);
-						let index = value;
-						while (result.indexOf('{@}') >= 0) {
-							result = result.replace("{@}", index.toString());
-							index++;
-						}
-						vscode.window.showInformationMessage((index - value).toString() + " Changed");
-						editor.edit(doc => {
-							doc.replace(textRange, result);
-						});
-						deactivate();
-					}
-				}
-				else
-				{
-					vscode.window.showErrorMessage("Not A Number");
-				}
-			})
+		let firstLine = editor.document.lineAt(0);
+		let lastLine = editor.document.lineAt(editor.document.lineCount - 1);
+		let textRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
+		let result = document.getText(textRange);
+		let index = startNumber;
+		while (result.indexOf('{@}') >= 0) {
+			result = result.replace("{@}", String(index).padStart(digitNumber, 0));
+			index+=stepNumber;
+		}
+		editor.edit(doc => {
+			doc.replace(textRange, result);
+		});
+		deactivate();
 
 	});
 
